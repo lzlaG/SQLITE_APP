8a74a718f5fdf198bd0a3747ca5f7aacb52de5e1
lzlaG <p0chta1234prosto@yandex.ru>
2024-05-17 00:32:35 +0500

    delete selected rows

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db = QSqlDatabase::addDatabase("QSQLITE"); // задаем драйвер для бд
 }
 
 MainWindow::~MainWindow()
@@ -24,9 +24,9 @@ void MainWindow::on_pushButton_clicked() // задание действия дл
 {
     QString fileName = QFileDialog::getOpenFileName(this,
             tr("Выберите SQLITE базу данных"), "", tr("Databases (*.db)")); // данная строка задает открытие диалогово окна с фильтром. Фильтр прописан чтобы показывались только файлы с расширением db
-    m_db.setDatabaseName(fileName);
+    m_db.setDatabaseName(fileName); // указываем путь к бд
     QMessageBox msgBox;
-    if (!m_db.open())
+    if (!m_db.open()) // проверка открытия базы
     {
         msgBox.setText("База данных не открылась. Проверьте корректность пути и файла");
         msgBox.exec();
@@ -35,14 +35,33 @@ void MainWindow::on_pushButton_clicked() // задание действия дл
     {
         msgBox.setText("Успех! База данных открылась");
         msgBox.exec();
-        //query = new QSqlQuery(m_db);
-        //query->exec("select * from Mutants;");
-
-        model = new QSqlTableModel(this,m_db);
-        model->setTable("Mutants");
-        model->select();
-        ui->tableView->setModel(model);
+        model = new QSqlTableModel(this,m_db); // инициализируем модель
+        model->setTable("Mutants"); // задаем таблицу для моедли
+        model->select(); // выбираем все из модели
+        ui->tableView->setModel(model); // передаем модель в tableView
+        ui->tableView->resizeColumnsToContents(); // делаем чтобы колонки совпадали по размеру с содержимым
     }
 }
 
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QModelIndexList selectedRows = ui->tableView->selectionModel()->selectedRows(); // сохраняем выделенные строки в массив
+    foreach (const QModelIndex &index, selectedRows) {
+        ui->tableView->model()->removeRow(index.row()); // удаляем строки в цикле
+    }
+    // перерисовываем таблицу с уже новыми данными
+    model = new QSqlTableModel(this,m_db);
+    model->setTable("Mutants");
+    model->select();
+    ui->tableView->setModel(model);
+    ui->tableView->resizeColumnsToContents();
+}
+
+
+// trash
+/*
+void MainWindow::on_tableView_clicked(const QModelIndex &index){};
+void MainWindow::on_pushButton_3_clicked(){};
+*/
