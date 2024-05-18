8b0544216bad57473d9e52a6b11866c6a61a0620
lzlaG <p0chta1234prosto@yandex.ru>
2024-05-18 23:27:12 +0500

    working tree

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,12 +4,14 @@
 #include <QtSql> // qt sql библиотека
 #include <QtGui>
 #include <QMessageBox>
+
 struct NodeData
 {
-    int id;
-    QString url;
+    QString id;
     QString date;
-    QString
+    QString url;
+    QString title;
+    QString visit_count;
 };
 
 MainWindow::MainWindow(QWidget *parent)
@@ -46,18 +48,47 @@ void MainWindow::on_pushButton_clicked() // задание действия дл
 
 void MainWindow::FillTree()
 {
-    QSqlQuery query("SELECT datetime(last_visit_time / 1000000 - 11644473600, 'unixepoch', 'localtime'),url,title,visit_count FROM urls;");
-
+    QSqlQuery query("SELECT id,datetime(last_visit_time / 1000000 - 11644473600, 'unixepoch', 'localtime'),url,title,visit_count FROM urls;");
+    if (!query.exec()) {
+        qWarning() << "Не удалось выполнить запрос";
+        return;
+    };
     model = new QStandardItemModel(this);
     rootItem = model->invisibleRootItem();
-    model->setColumnCount(2);
+    model->setColumnCount(5);
+    int i = 0;
+    while(query.next())
+    {
+        NodeData OurData;
+        OurData.id = query.value(0).toString();
+        OurData.date = query.value(1).toString();
+        OurData.url = query.value(2).toString();
+        OurData.title = query.value(3).toString();
+        OurData.visit_count = query.value(4).toString();
+
+        QStandardItem *qid = new QStandardItem(OurData.id);
+        QStandardItem *qday = new QStandardItem(OurData.date);
+        QStandardItem *qurl = new QStandardItem(OurData.url);
+        QStandardItem *qtitle = new QStandardItem(OurData.title);
+        QStandardItem *qvisit_count = new QStandardItem(OurData.visit_count);
+
+        model->setItem(i, 0,qid);
+        model->setItem(i, 1, qday);
+        model->setItem(i, 2, qurl);
+        model->setItem(i, 3, qtitle);
+        model->setItem(i, 4, qvisit_count);
+        i+=1;
+    }
+
+    //расскоментировать блок ниже если интересен тест с большим количеством записей
+    /*
     for (int i=0; i<1000000; i++)
     {
         QStandardItem *item1 = new QStandardItem(QString("Столбец 1, строка %1").arg(i));
         QStandardItem *item2 = new QStandardItem(QString("Столбец 2, строка %1").arg(i));
-        model->setItem(i, 0, 1);
+        model->setItem(i, 0, item1);
         model->setItem(i, 1, item2);
-    };
+    };*/
 };
 void MainWindow::on_debug_button_clicked()
 {
